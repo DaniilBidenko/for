745b547fadf90b0e517a78a3ed0ff06b6eb3a138
User PC <daniilbidenko5@gmail.com>
2025-01-21 15:39:14 +0600

    Прошли циклы, перешли к новой части

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+void main () {
+  // for(Инициализация; Условие; Изменение) {
+      // Тело цикла
+  // }
+
+  // for (int i = 0; i < 10; i++){
+  //   print(i);
+  // }
+
+  // for (int i = 10; i >= 0; i--){
+  //   print(i);
+  // }
+
+  // for (int i = 0; i <= 20; i++) {
+  //   if (i % 2 != 0) {
+  //     print(i);
+  //   }
+  // }
+
+  final List familyName = [
+    
+    'Даниил',
+    'Ева',
+    'Елена',
+    'Виктор',
+    'Светлана',
+    'Ангелина',
+    'Даниил',
+    'Ира',
+  ];
+
+  // print(familyName[3]);
+
+  // for (int i = 0; i < familyName.length; i++) {
+  //   print(familyName[i]);
+  // } 
+
+
+  // for (int i = 0; i < familyName.length; i++) {
+  //   print('$i - ${familyName[i]}');
+  // }
+
+  // for (int i = familyName.length - 1; i >= 0; i--) {
+  //   print('$i - ${familyName[i]}');
+  // }
+
+
+      // for (int i = 0;i < familyName.length; i +=3) {
+      //   print(familyName[i]);
+      // }
+
+    
+}
+
